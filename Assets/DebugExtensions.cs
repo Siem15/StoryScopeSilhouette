43d579d36746b735311bcb317196178fe6cb7f89
ldebont <90321941+ldebont@users.noreply.github.com>
2024-05-14 10:35:00 +0200

    Added 'DebugExtensions'-script to combine various debug scripts.

---
+++ Assets/DebugExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Stores all code for debugging application.
+/// </summary>
+public class DebugExtensions : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
