f6272376224de4b671df179de678bb81f923db94
Siem15 <siem.wesseling@hva.nl>
2024-05-29 14:47:19 +0200

    Shader changes v2

---
+++ Assets/Silhouette/Scripts/ShaderManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// In this script materials are called on the moment that objects with the required properties interact with eachother.
+/// 
+/// - Siem Wesseling, 29/05/2024
+/// </summary>
+
+public class ShaderManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddShader()
+    {
+
+    }
+}
