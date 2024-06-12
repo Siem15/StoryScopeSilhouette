f63b7e06b9e427674b5b7710aed7424e63ac1016
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2024-06-12 14:45:35 +0200

    BananaPeel Object

---
+++ Assets/Silhouette/Scripts/BananaPeel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaPeel : MonoBehaviour
+{
+    [SerializeField] float spinDuration = 1f;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            StartCoroutine(Rotate(collision.gameObject, spinDuration));
+        }
+    }
+
+    IEnumerator Rotate(GameObject rObject, float duration)
+    {
+        float startRotation = rObject.transform.eulerAngles.z;
+        float endRotation = startRotation + 360.0f;
+        float t = 0.0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float zRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+            rObject.transform.eulerAngles = new Vector3(rObject.transform.eulerAngles.x, rObject.transform.eulerAngles.y, zRotation);
+            yield return null;
+        }
+        rObject.transform.eulerAngles = new Vector3(rObject.transform.eulerAngles.x, rObject.transform.eulerAngles.y, 0);
+    }
+}
